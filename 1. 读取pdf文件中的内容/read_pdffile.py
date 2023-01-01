9e8c4bd57968155ad035150886f3b0b5fddabf82
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-01-01 22:24:00 +0900

    Create read_pdffile.py

---
+++ 1. 读取pdf文件中的内容/read_pdffile.py
@@ -0,0 +1,20 @@
+import PyPDF2
+
+#  python读取pdf文件
+# https://blog.csdn.net/m0_56636447/article/details/127539726
+# ok https://www.post.japanpost.jp/zipcode/dl/bangobo/02.pdf
+# pip install PyPDF2=="2.10.0"
+
+
+
+import PyPDF2
+
+#  python读取pdf文件
+# https://blog.csdn.net/m0_56636447/article/details/127539726
+# ok https://www.post.japanpost.jp/zipcode/dl/bangobo/02.pdf
+pdfFileObj = open("02.pdf","rb")
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+for item in range(pdfReader.numPages):
+    pageObj = pdfReader.getPage(item)
+    print(pageObj.extractText())
