26e45287d58e7e78c3b32f58d309902a9249ec3d
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-02-03 09:47:03 +0900

    Create read_excel.py

---
+++ 10.读取excel文件每一行数据，然后转向输出写入tsv/read_excel.py
@@ -0,0 +1,35 @@
+
+
+
+import openpyxl
+
+from pandas.core.frame import DataFrame
+import pandas as pd
+import xlrd
+
+import csv
+
+
+def writeinto_detail(filename, data):
+    with open(filename, "a", newline="", encoding="utf-8") as f:
+        csv_out = csv.writer(f, delimiter=",")
+        csv_out.writerow(data)
+
+
+def output_list(list_item):
+    for item in list_item:
+        writeinto_detail(text_file,[item])
+
+    writeinto_detail(text_file,["--"*66])
+
+def read_excel(excelfile):
+    file_dt = pd.read_excel(excelfile)
+    for item in file_dt.values.tolist():
+        output_list(item)
+
+
+if __name__ =="__main__":
+    excelfile = "d.xlsx"
+    text_file = "dd.tsv"
+
+    read_excel(excelfile)
